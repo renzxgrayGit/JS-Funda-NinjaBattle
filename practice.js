d36cd7925f8db94280e50e535e019ff116c921b2
renzxgrayGit <renzocabajar9@gmail.com>
2024-03-02 11:03:34 +0800

    update

---
+++ practice.js
@@ -0,0 +1,28 @@
+var ninja1 = {
+    hp: 100,
+    strength: 15,
+    attack: function() {
+        return Math.floor(Math.random() * this.strength + 1);
+    }
+}
+
+var ninja2 = {
+    hp: 150,
+    strength: 7,
+    attack: function() {
+        return Math.floor(Math.random() * this.strength + 1);
+    }
+}
+
+for(let i = 1; i <= 10; i++) {
+    const ninja1_dmg = ninja1.attack();
+    const ninja2_dmg = ninja2.attack();
+
+    ninja1.hp -= ninja2_dmg;
+    ninja2.hp -= ninja1_dmg;
+
+    console.log(`===Round ${i}===`);
+    console.log(`Ninja1 attacks Ninja2 and does a damage of${ninja1_dmg}! Ninja1 HP:${ninja1.hp}`);
+    console.log(`Ninja2 attacks Ninja1 and does a damage of${ninja2_dmg}! Ninja2 HP:${ninja2.hp}`);
+}
+
